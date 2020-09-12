a4fd4ef2e7ea3a8a80578aa5b32445a69c02190e
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-11 21:35:03 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,30 @@
 use employee_tracker_db;
 
 -- Departments --
-insert into departments (department_name) values ("Engineering");
-insert into departments (department_name) values ("Sales");
-insert into departments (department_name) values ("Finance");
-insert into departments (department_name) values ("Legal");
-insert into departments (department_name) values ("None");
+INSERT INTO departments (department_name) VALUES ("Engineering"),
+INSERT INTO departments (department_name) VALUES ("Sales"),
+INSERT INTO departments (department_name) VALUES ("Finance"),
+INSERT INTO departments (department_name) VALUES ("Legal"),
+INSERT INTO departments (department_name) VALUES ("None")
 
 -- Roles -- 
-insert into roles (title, salary, department_id) ("Lead Engineer",200000,1);
-insert into roles (title, salary, department_id) ("Software Engineer",130000,1);
-insert into roles (title, salary, department_id) ("Sales Lead",10000,2);
-insert into roles (title, salary, department_id) ("Salesperson",40000,2);
-insert into roles (title, salary, department_id) ("Accountant", 200000, 3);
-insert into roles (title, salary, department_id) ("legal Team Lead", 200000, 4);
-insert into roles (title, salary, department_id) ("Lawyer", 200000, 4);
-insert into roles (title, salary, department_id) ("None", 0, 5);
+INSERT INTO roles (title, salary, department_id) VALUES ("Lead Engineer",200000,1),
+INSERT INTO roles (title, salary, department_id) VALUES ("Software Engineer",130000,1),
+INSERT INTO roles (title, salary, department_id) VALUES ("Sales Lead",10000,2),
+INSERT INTO roles (title, salary, department_id) VALUES ("Salesperson",40000,2),
+INSERT INTO roles (title, salary, department_id) VALUES ("Accountant", 200000, 3),
+INSERT INTO roles (title, salary, department_id) VALUES ("legal Team Lead", 200000, 4),
+INSERT INTO roles (title, salary, department_id) VALUES ("Lawyer", 200000, 4),
+INSERT INTO roles (title, salary, department_id) VALUES ("None", 0, 5)
 
 -- Employees --
-insert into employees (first_name, last_name, role_id, manager_id) values ("Rosie","Riviter", 1, null);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Sophia","Laren", 2, null);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Eric","Churchill",3, null);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Fish","Oaks",4, null);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Mia","Rogers", 2, 1);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Nasi","Ein", 4, 2);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Nina","Alaska", 2, 1);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Two Toes","Boots",4,2);
-insert into employees (first_name, last_name, role_id, manager_id) values ("Joe","Wright",7,4);
-insert into employees (first_name, last_name, role_id) values ("None"," ",8);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Rosie","Riviter", 1, null),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Sophia","Laren", 2, null),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Eric","Churchill",3, null),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Fish","Oaks",4, null),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Mia","Rogers", 2, 1),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Nasi","Ein", 4, 2),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Nina","Alaska", 2, 1),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Two Toes","Boots",4,2),
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Joe","Wright",7,4),
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("Navine","Rocky",8)
